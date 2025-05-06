d391a45eb637b5ef870a903407549106dc1cb498
Isabella <isabellalucena15@gmail.com>
2025-05-06 13:14:08 -0300

    dia 19

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{ // its not necessary due to Laravel already having a 'Route::resource' method
+  //what it does is create all the routes for the resource controller
+  //when using RESTful or RESTful-like routes
+    public function index(){
+        $jobs = Job::with('employer')->latest()->simplePaginate(4); //eager loading - reduce sql queries.
+
+        return view('jobs.index', [
+            'jobs' => $jobs
+        ]);
+    }
+
+    public function create(){
+        return view('jobs.create');
+    }
+
+    public function show(Job $job){
+        return view('jobs.show', ['job' => $job]);
+    }
+
+    public function store(){
+        request()->validate([
+            'title' => ['required', 'min:3'],
+            'salary' => ['required']
+        ]);
+
+        Job::create([
+            'title' => request('title'),
+            'salary' => request('salary'),
+            'employer_id' => 1
+        ]);
+
+        return redirect('/jobs');
+    }
+
+    public function edit(Job $job){
+        return view('jobs.edit', [
+            'job' => $job
+        ]);
+    }
+
+    public function update(Job $job){
+        //authorize (On hold)
+
+        request()->validate([
+            'title' => ['required', 'min:3'],
+            'salary' => ['required']
+        ]);
+
+
+        $job->update([
+            'title' => request('title'),
+            'salary' => request('salary')
+        ]);
+
+        return redirect('/jobs/'. $job->id);
+    }
+
+    public function destroy(Job $job){
+        //authorize - on hold
+
+        $job->delete();
+
+        return redirect('/jobs');
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,97 +1,12 @@
 <?php
 
-use App\Models\Job;
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
+
+Route::resource('jobs', JobController::class);
 
 
-Route::get('/', function () {
-    return view('home'
-    );
-});
-
-
-// Index
-Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->latest()->simplePaginate(4); //eager loading - reduce sql queries.
-
-    return view('jobs.index', [
-        'jobs' => $jobs
-    ]);
-});
-
-// Create
-Route::get('/jobs/create', function () {
-    return view('jobs.create');
-});
-
-
-// Show
-Route::get('/jobs/{id}', function ($id) {
-    $job = Job::find($id);
-
-    return view('jobs.show', [
-        'job' => $job
-    ]);
-});
-
-// Store
-Route::post('/jobs', function () {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    Job::create([
-        'title' => request('title'),
-        'salary' => request('salary'),
-        'employer_id' => 1
-    ]);
-
-    return redirect('/jobs');
-});
-
-// Edit
-Route::get('/jobs/{id}/edit', function ($id) {
-    $job = Job::find($id);
-
-    return view('jobs.edit', [
-        'job' => $job
-    ]);
-});
-
-// Update
-Route::patch('/jobs/{id}', function ($id) {
-    request()->validate([
-        'title' => ['required', 'min:3'],
-        'salary' => ['required']
-    ]);
-
-    //authorize (On hold)
-
-    //update the job
-
-    $job = Job::findOrFail($id);
-
-    $job->update([
-        'title' => request('title'),
-        'salary' => request('salary')
-    ]);
-
-    return redirect('/jobs/'. $job->id);
-
-});
-
-// Destroy
-Route::delete('/jobs/{id}', function ($id) {
-    //authorize - on hold
-
-    Job::findOrFail($id)->delete();
-
-    return redirect('/jobs');
-});
-
-Route::get('/contact', function() {
-    return view('contact');
-});
 
