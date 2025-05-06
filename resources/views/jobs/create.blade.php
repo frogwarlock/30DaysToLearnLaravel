43774b1a8820348bca05993003269b444c449db5
Isabella <isabellalucena15@gmail.com>
2025-05-06 14:17:46 -0300

    teste

--- resources/views/jobs/create.blade.php
+++ resources/views/jobs/create.blade.php
@@ -11,15 +11,13 @@
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
+                        <x-form-label for="title">Title</x-form-label>
                         <div class="mt-2">
                             <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                                 <input type="text" name="title" id="title" class="block min-w-0 grow py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" placeholder="Shift Leader" required>
                             </div>
 
-                            @error('title')
-                            <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
-                            @enderror
+                            <x-form-error name="title"></x-form-error>
                         </div>
                     </div>
 
@@ -35,15 +33,6 @@
                         <p class="text-xs text-red-500 font-semibold mt-1">{{ $message }}</p>
                         @enderror
 
-{{--                        <div class="mt-10">--}}
-{{--                            @if($errors->any())--}}
-{{--                                <ul>--}}
-{{--                                    @foreach($errors->all() as $error)--}}
-{{--                                        <li class="text-red-500 italic">{{ $error }}</li>--}}
-{{--                                    @endforeach--}}
-{{--                                </ul>--}}
-{{--                            @endif--}}
-{{--                        </div>--}}
                     </div>
 
 
