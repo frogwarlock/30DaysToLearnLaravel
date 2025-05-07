d23979ada15ecdd4cade64aaa57d4862509d4475
Isabella <isabellalucena15@gmail.com>
2025-05-07 14:04:56 -0300

    implementacao ate dia 22

--- resources/views/jobs/edit.blade.php
+++ resources/views/jobs/edit.blade.php
@@ -10,14 +10,14 @@
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                     <div class="sm:col-span-4">
-                        <label for="title" class="block text-sm/6 font-medium text-gray-900">Title</label>
+                        <label for="title" class="block text-sm leading-6 font-medium text-gray-900">Title</label>
                         <div class="mt-2">
                             <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                                 <input
                                     type="text"
                                     name="title"
                                     id="title"
-                                    class="block min-w-0 grow py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                                    class="block min-w-0 grow py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm leading-6"
                                     placeholder="Shift Leader"
                                     value="{{ $job->title }}"
                                     required>
@@ -30,14 +30,14 @@
                     </div>
 
                     <div class="sm:col-span-4">
-                        <label for="salary" class="block text-sm/6 font-medium text-gray-900">Salary</label>
+                        <label for="salary" class="block text-sm leading-6 font-medium text-gray-900">Salary</label>
                         <div class="mt-2">
                             <div class="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
                                 <input
                                     type="text"
                                     name="salary"
                                     id="salary"
-                                    class="block min-w-0 grow py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                                    class="block min-w-0 grow py-1.5 pr-3 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm leading-6"
                                     placeholder="$50,000 Per Year"
                                     value="{{ $job->salary }}"
                                     required>
@@ -51,8 +51,6 @@
                     </div>
                 </div>
             </div>
-
-
         </div>
 
         <div class="mt-6 flex items-center justify-between gap-x-6">
@@ -63,7 +61,7 @@
             </div>
 
             <div class="flex items-center gap-x-6">
-                <a href="/jobs/{{ $job->id }}"  class="text-sm/6 font-semibold text-gray-900">Cancel</a>
+                <a href="/jobs/{{ $job->id }}" class="text-sm leading-6 font-semibold text-gray-900">Cancel</a>
                 <button type="submit"
                         class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                     Save
@@ -75,7 +73,5 @@
     <form method="POST" action="/jobs/{{ $job->id }}" id="delete-form" class="hidden">
         @csrf
         @method('DELETE')
-
     </form>
-
 </x-layout>
